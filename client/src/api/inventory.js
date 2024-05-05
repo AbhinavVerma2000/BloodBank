8e00614e8e702022360d4a1572832bf1b24c22e5
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-06 00:34:28 +0530

    Update inventory.js

--- client/src/api/inventory.js
+++ client/src/api/inventory.js
@@ -1,16 +1,16 @@
 import { axiosInstance } from ".";
 export const AddInventory = (data)=>{
-    return axiosInstance('post', `${process.env.REACT_APP_BACK}/api/inventory/add`,data)
+    return axiosInstance('post', `/api/inventory/add`,data)
 }
 export const GetInventory = ()=>{
-    return axiosInstance('get',`${process.env.REACT_APP_BACK}/api/inventory/get`)
+    return axiosInstance('get',`/api/inventory/get`)
 }
 export const GetAllDonorOrg = ()=>{
-    return axiosInstance('get',`${process.env.REACT_APP_BACK}/api/users/getalldonors`)
+    return axiosInstance('get',`/api/users/getalldonors`)
 }
 export const GetAllHos = ()=>{
-    return axiosInstance('get',`${process.env.REACT_APP_BACK}/api/users/getallhospitals`)
+    return axiosInstance('get',`/api/users/getallhospitals`)
 }
 export const GetInvwithFilter = (data)=>{
-    return axiosInstance('get',`${process.env.REACT_APP_BACK}/api/inventory/filter`, data)
+    return axiosInstance('get',`/api/inventory/filter`, data)
 }
