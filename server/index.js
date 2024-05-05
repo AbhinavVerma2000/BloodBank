327e0341537fc2236bcfa0ec947fc4913e66716c
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-05 21:06:13 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+const dbconfig = require('./dbconfig')
+app.use(cors())
+app.use(express.json())
+const usersRoute = require('./routes/usersRoute')
+const invRoute = require('./routes/invRoute')
+const dashboardRoute = require('./routes/dashboardRoute')
+app.use('/api/users', usersRoute)
+app.use('/api/inventory', invRoute)
+app.use('/api/dashboard', dashboardRoute)
+const path = require('path')
+__dirname = path.resolve()
+if (process.env.NODE_ENV==="production") {
+    app.use(express.static(path.join(__dirname,'/client/build')))
+    app.get('*',(req, res)=>{
+        res.sendFile(path.join(__dirname, 'client','build','index.html'))
+    })
+}
+app.listen(port, ()=>console.log(`Server started at ${port}`))
