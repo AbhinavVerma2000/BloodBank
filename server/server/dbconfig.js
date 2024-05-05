416d361bc4283fcec19dd3169631488f7cc0428b
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-05 20:02:31 +0530

    Delete dbconfig.js

--- server/server/dbconfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI)
-const connection = mongoose.connection
-connection.on('connected',()=>{
-    console.log('Mongodb connection successful')
-})
-connection.on('error',(err)=>{
-    console.log('Mongodb connection error', err)
-})
