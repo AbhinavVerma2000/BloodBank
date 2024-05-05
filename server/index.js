7d1e298f1a166ad39e1cb986c7cd168e1e0e626f
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-05 23:43:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 const dbconfig = require('./dbconfig')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://blood-bank-seven-weld.vercel.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
+app.use(cors())
 app.use(express.json())
 const usersRoute = require('./routes/usersRoute')
 const invRoute = require('./routes/invRoute')
