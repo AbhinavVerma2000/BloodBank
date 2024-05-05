149a511395996bee8bde07136abee1fcfa0a87eb
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-06 00:37:33 +0530

    Update dashbrd.js

--- client/src/api/dashbrd.js
+++ client/src/api/dashbrd.js
@@ -1,4 +1,4 @@
 import { axiosInstance } from ".";
 export const Getbloodgrpdata = ()=>{
-    return axiosInstance('get',`${process.env.REACT_APP_BACK}/api/dashboard/bloodgrpdata`)
+    return axiosInstance('get',`/api/dashboard/bloodgrpdata`)
 }
