60607d04f6d1192a2b51c002f4920e6175cd34a2
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-05 19:40:01 +0530

    Create index.js

---
+++ client/src/pages/Profile/Donor/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { Setloading } from '../../../redux/loaderSlice';
+import { GetAllDonorOrg} from '../../../api/inventory';
+import { Table, message } from 'antd';
+import moment from 'moment';
+
+const Donor = () => {
+    const [data, setData] = React.useState([]);
+    const dispatch = useDispatch()
+    const getData = async () => {
+        try {
+          dispatch(Setloading(true));
+          const response = await GetAllDonorOrg();
+          dispatch(Setloading(false));
+          if (response.success) {
+            setData(response.data);
+          } else {
+            throw new Error(response.message);
+          }
+        } catch (error) {
+          message.error(error.message);
+          dispatch(Setloading(false));
+        }
+      };
+      const columns = [{
+        title: "Name",
+        dataIndex: "name"
+      },
+    {
+        title: "Email",
+        dataIndex: "email"
+    },{
+        title: "Phone",
+        dataIndex: "phone"
+    },{
+        title: "Created At",
+        dataIndex: "createdAt",
+        render: (date)=>moment(date).format('DD MMM YYYY hh:mm A')
+    }]
+      React.useEffect(() => {
+        getData();
+      },[]);
+  return (
+    <div>
+      <Table columns={columns} dataSource={data}/>
+    </div>
+  )
+}
+
+export default Donor
