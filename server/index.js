654debb79cb8ef7f3b4c7ac896e3ef80535845f3
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-05-05 21:23:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,5 +11,12 @@ const dashboardRoute = require('./routes/dashboardRoute')
 app.use('/api/users', usersRoute)
 app.use('/api/inventory', invRoute)
 app.use('/api/dashboard', dashboardRoute)
-
+// const path = require('path')
+// __dirname = path.resolve()
+// if (process.env.NODE_ENV==="production") {
+//     app.use(express.static(path.join(__dirname,'/client/build')))
+//     app.get('*',(req, res)=>{
+//         res.sendFile(path.join(__dirname, 'client','build','index.html'))
+//     })
+// }
 app.listen(port, ()=>console.log(`Server started at ${port}`))
